fa6c992b0b10f9c7a3b8125bad7458e9829fc19c
Omar Alfonso García Santana <omardpana22@gmail.com>
2020-05-05 15:09:39 +0200

    activating quote file

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def primary():
-  print("Keep it logically awesome.");
+  #print("Keep it logically awesome.");
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes)
 
 if __name__== "__main__":
   primary()
